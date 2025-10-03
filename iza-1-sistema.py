216339baa1d0a79f99a39b5016c3e52d1b5dfdad
viniciusstack-ops <vinicius.stack@escola.pr.gov.br>
2025-10-03 07:59:17 -0300

    Add files via upload

---
+++ iza-1-sistema.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+print("*** Mini Questionário ***\n")
+nome = input("Qual seu nome? ")
+print("Qual sua idade?\n")
+genero = input("Você é homen ou mulher?")
+endereco = input("Qual o seu endereço?\n")
+
+os.system("cls")
+
+print("*** Mini Questionário ***\n")
+print("Olá, ", nome, "\n")
+print("Idade: ", idade, "\n")
+print("Genero: ", genero, "\n")
+print("Endereço: ", endereco, "\n")
